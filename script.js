bd9ecaa4fa9ae3042bc46769ca060a0204b93632
ragul2k23 <rockyragulsiva2005@gmail.com>
2023-11-15 21:21:03 +0530

    Everything encapsulated with class
    One cannot access anything outside the class
    and modify the contents(i.e book array etc)
    
    Will add some visual treat while learning local storage.

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-
-/* DOM MANIPULATIONS */
-const bookContainer = document.querySelector('div.booksContainer')
-const dialog        = document.getElementById("bookInfo");
-const newBookBtn    = document.getElementById("newBook")
-const updateButton  = document.getElementById("update");
-const cancelButton  = document.getElementById("cancel");
  
 class Book{
     
     static #myLibrary =[];
     static #card;static #readStatusBtn;
     static #readStatusButtons;static #rmvbtns;
+    static #dialog  = document.getElementById("bookInfo");
+    static #bookContainer = document.querySelector('div.booksContainer')
+    static #newBookBtn    = document.getElementById("newBook")
+    static #updateButton  = document.getElementById("update");
+    static #cancelButton  = document.getElementById("cancel");
+
     constructor(title,author,pages,read){
     this.title = title;
     this.author = author;
@@ -19,8 +18,11 @@ class Book{
     this.info= function(){
         return `The   ${this.title} by ${this.author} ,${this.pages}pages,${this.read}`;
        }
+       Book.#updateButton.addEventListener('click',Book.createNewBooks) 
    
    }
+
+   
 
    static createReadStatusBtn(book){
     Book.#readStatusBtn = document.createElement("button");
@@ -60,21 +62,23 @@ class Book{
 
   }
 
-  static addEvents(){ //fnc to add evnts for newly joined books too 
-    this.forEach( (btn)=>{
+static addEvents()   { //fnc to add evnts for newly joined books too 
+    Book.#rmvbtns.forEach( (btn)=>{
         btn.addEventListener("click",()=> Book.removeBook(btn))
     } )
     Book.#readStatusButtons.forEach((btn)=>{
         btn.addEventListener("click",()=> Book.changeReadStatus(btn))
     })
 
-    /*Handling opening and closing of dialog box*/
-    newBookBtn.addEventListener("click", () => {
-    dialog.show();
-    cancelButton.addEventListener("click",()=>{
-    dialog.close(); 
+    /*Handling opening and closing of Book.#dialog box*/
+    console.log(this)
+    Book.#newBookBtn.addEventListener("click", () => {
+    Book.#dialog.show();
+    Book.#cancelButton.addEventListener("click",()=>{
+    Book.#dialog.close(); 
         })
-        });
+      });
+
     }
 
     
@@ -94,40 +98,7 @@ static addBookToLibrary(...args) {
     Book.displayBooks();
  }  
 
-   static displayBooks() { 
-    //console.log(Book.#myLibrary)
-    bookContainer.textContent='';/*free container*/
-    Book.#myLibrary.forEach( (book)=> { 
-        
-        Book.#card = document.createElement('div');
-        Book.#card.classList=("card");
-        bookContainer.appendChild(Book.#card);
-        Book.#card.textContent = book.info(); 
-        Book.createRemoveBtn(book);/*creating rmv btns*/ 
-        Book.createReadStatusBtn(book);
-        
-        console.log(Book.#readStatusBtn,'dfd');
-        console.log(book.read)
-         })
-    Book.#rmvbtns = document.querySelectorAll("[data-book]"); 
-    Book.#readStatusButtons = document.querySelectorAll("button[data-read]");
-    // changeReadStatus()
-    Book.addEvents.call(Book.#rmvbtns);//add event listenr for readstatubtn too
- }
-
-}     
-
-    //example books
-    const physics = new Book("pysic","rocky","464","read");
-    const chemistry = new Book("cmstry","rocky","443","read");
-    const ds = new Book("datastructrue","siva siva","8834","notRead");
-    const maths = new Book("maths","rocky","348295","read");
-    Book.addBookToLibrary(maths,physics,chemistry,ds);
-
-    
-
-
-updateButton.addEventListener('click',function() { 
+ static createNewBooks() { 
     /*getting form inputs*/
     const bookTitle     = document.getElementById("title");
     const bookPages     = document.getElementById("pages");
@@ -140,9 +111,43 @@ updateButton.addEventListener('click',function() {
     /*creating new books */
     newObj = new Book(`${bookTitle.value}`,`${bookAuthor.value}`,`${bookPages.value}`,`${bookReadStatus}`)
     Book.addBookToLibrary(newObj);
-    Book.displayBooks();dialog.close();
+    Book.displayBooks();
     
-}) 
+}
 
+   static displayBooks() { 
+    //console.log(Book.#myLibrary)
+    Book.#dialog.close();
+    Book.#bookContainer.textContent='';/*free container*/
+    Book.#myLibrary.forEach( (book)=> { 
+        
+        Book.#card = document.createElement('div');
+        Book.#card.classList=("card");
+        Book.#bookContainer.appendChild(Book.#card);
+        Book.#card.textContent = book.info(); 
+        Book.createRemoveBtn(book);/*creating rmv btns*/ 
+        Book.createReadStatusBtn(book);
+        
+        console.log(Book.#readStatusBtn,'dfd');
+        console.log(book.read)
+         })
+    Book.#rmvbtns = document.querySelectorAll("[data-book]"); 
+    Book.#readStatusButtons = document.querySelectorAll("button[data-read]");
+    // changeReadStatus()
+    Book.addEvents();//add event listenr for readstatubtn too
+ }
 
+}     
+
+    //example books
+    const physics = new Book("pysic","rocky","464","read");
+    const chemistry = new Book("cmstry","rocky","443","read");
+    const ds = new Book("datastructrue","siva siva","8834","notRead");
+    const maths = new Book("maths","rocky","348295","read");
+    Book.addBookToLibrary(maths,physics,chemistry,ds);
+
+    
+    
+     
+    
  
